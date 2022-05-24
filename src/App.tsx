90a6e4ca35873e194895c1798917843c6bc42ea7
Josh Rodriguez <jrodshua@gmail.com>
2022-05-24 16:01:44 -0700

    Add react router, wrap app with react-query and router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="App">
-      <h1>anime park</h1>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <h1>anime park</h1>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
